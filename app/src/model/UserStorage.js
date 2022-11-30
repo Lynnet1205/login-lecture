9e2913e26277ad07f5d9f241839acb01903682e1
Lynnet <h.ring1205@gmail.com>
2022-11-30 18:34:53 +0900

    회원가입 라우팅 구현

--- app/src/model/UserStorage.js
+++ app/src/model/UserStorage.js
@@ -3,7 +3,7 @@
 class UserStorage {
   static #users = {
     id: ["woorimIT", "나개발", "김팀장"],
-    psword: ["1234", "1234", "123456"],
+    pwd: ["1234", "1234", "123456"],
     name: ["우리밋", "나개발", "김팀장"],
   };
 
@@ -21,13 +21,20 @@ class UserStorage {
   static getUserInfo(id) {
     const users = this.#users;
     const idx = users.id.indexOf(id);
-    const usersKeys = Object.keys(users); // => [id, psword, name]
+    const usersKeys = Object.keys(users); // => [id, pwd, name]
     const userInfo = usersKeys.reduce((newUser, info) => {
       newUser[info] = users[info][idx];
       return newUser;
     }, {});
 
     return userInfo;
+  }
+  static save(userInfo){
+    const users = this.#users;
+    users.id.push(userInfo.id);
+    users.name.push(userInfo.name);
+    users.pwd.push(userInfo.pwd);
+    console.log(users);
   }
 }
 
