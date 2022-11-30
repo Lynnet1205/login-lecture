cd5c13014e3c3a80413a84dd4f0d4d1b0b5934ee
Lynnet <h.ring1205@gmail.com>
2022-11-30 15:17:51 +0900

    로그인을 요청하는 fetch생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -5,11 +5,20 @@ const id = document.querySelector("#id"),
     loginBtn = document.querySelector("button");
 
 
-    loginBtn.addEventListener("click", login);
+loginBtn.addEventListener("click", login);
 
-    function login() {
-        const req = {
-            id: id.value,
-            pwd: pwd.value,
-        };
-    }
+function login() {
+    const req = {
+        id: id.value,
+        pwd: pwd.value,
+    };
+
+    fetch("/login", {
+        method: "POST",
+        header:{
+            "Content-Type" : "application/json",
+        },
+        body: JSON.stringify(req)
+    })
+
+}
