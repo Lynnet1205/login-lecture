810082d3309c2c3828908698254856ba71202c5d
Lynnet <h.ring1205@gmail.com>
2022-11-30 15:47:06 +0900

    백앤드에서 로그인 기능 구형

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,17 +1,42 @@
 "use strict";
 //urㅣ 로 옮긴 후 보여주는 기능을 해주는 module
-
-const home = (req,res) =>{
-    res.render("home/index")
- };
-
- const login = (req,res)=>{
-    res.render("home/login")  
+const users = {
+    id: ["lynnet", "bruno", "ian"],
+    pwd: ["1234","12345", "123456"],
 };
 
-module.exports={
-    home,
-    login,
+const output = {
+    home: (req, res) => {
+        res.render("home/index")
+    }, 
+
+    login: (req, res) => {
+        res.render("home/login")
+    },
+}
+
+const process = {
+    login: (req,res) => {
+       const id = req.body.id,
+        pwd = req.body.pwd;
+
+     if(users.id.includes(id)){
+        const idx =users.id.indexOf(id);
+        if(users.pwd[idx] ===pwd){
+            return res.json({
+                success: true,
+            })
+        }
+     }
+     return res.json({
+        msg: "로그인에 실패했습니다.",
+     })
+    },
+}
+
+module.exports = {
+    output,
+    process,
 };
 
 /*👉 같은 것
